f413e2483332f6379720de372199411384b94cc0
lullasy <cid378@gmail.com>
2017-01-23 01:58:57 +0900

    データ取得とか　5.2まで

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -13,4 +13,39 @@ class RecordController < ApplicationController
     @books = Book.where('publish = ? AND price >= ?', params[:publish], params[:price])
     render 'hello/list'
   end
+
+  def order
+    @books = Book.where(publish: '技術評論社').order(price: :desc)
+    render 'hello/list'
+  end
+
+  def page
+    page_size = 3
+    page_num = params[:id] == nil ? 0 : params[:id].to_i - 1
+    @books = Book.order(published: :desc).limit(page_size).offset(page_size * page_num)
+    render 'hello/list'
+  end
+
+  def groupby
+    @books = Book.select('publish, AVG(price) AS avg_price').group(:publish)
+  end
+
+  def havingby
+    @books = Book.select('publish, AVG(price) AS avg_price').group(:publish).having('AVG(price) >= ?', 2500)
+    render 'record/groupby'
+  end
+
+  def none
+    case params[:id]
+    when 'all'
+      @books = Book.all
+    when 'new'
+      @books = Book.order('published DESC').limit(5)
+    when 'cheap'
+      @books = Book.order(:price).limit(5)
+    else
+      @books = Book.none
+    end
+    render 'books/index'
+  end
 end
