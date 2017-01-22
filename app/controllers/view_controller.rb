e1c1d05555d9e4aa40b008ba83757cb9b605d49d
lullasy <cid378@gmail.com>
2017-01-22 22:36:28 +0900

    4章おわってませんでした　partialらへん

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -7,4 +7,20 @@ class ViewController < ApplicationController
   def fields
     @user = User.find(1)
   end
+
+  def partial_basic
+    @book = Book.find(2)
+  end
+
+  def partial_param
+    @book = Book.find(2)
+  end
+
+  def partial_col
+    @books = Book.all
+  end
+
+  def partial_spacer
+    @books = Book.all
+  end
 end
